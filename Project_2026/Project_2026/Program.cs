de13793056a29c4b4add0f7cdeed4600a500bcaf
Matwiee <mateuszwiercioch0@gmail.com>
2026-05-06 17:35:22 +0200

    Revert "feat: implement MVC pattern with controllers and views"
    This reverts commit b10e974045287bf8852e6989d713198445621d54.

--- Project_2026/Project_2026/Program.cs
+++ Project_2026/Project_2026/Program.cs
@@ -1,6 +1,2 @@
-﻿var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
-app.Run();
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
