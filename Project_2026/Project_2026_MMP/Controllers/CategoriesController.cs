b10e974045287bf8852e6989d713198445621d54
Matwiee <mateuszwiercioch0@gmail.com>
2026-05-06 17:34:18 +0200

    feat: implement MVC pattern with controllers and views

---
+++ Project_2026/Project_2026_MMP/Controllers/CategoriesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project_2026_MMP.Models;
+
+namespace Project_2026_MMP.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            var category = new Category { CategoryId = id.HasValue?id.Value : 0  };
+
+            return View(category);
+
+        }
+
+    }
+}
---
+++ Project_2026/Project_2026_MMP/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project_2026_MMP.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       
+    }
+}
