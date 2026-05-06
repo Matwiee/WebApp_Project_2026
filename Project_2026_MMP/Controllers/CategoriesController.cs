c1bafdbb4ea3e8548522b25ff1417324fe8768ed
Matwiee <mateuszwiercioch0@gmail.com>
2026-05-06 20:51:11 +0200

    feat: implement model binding in HttpGet action

--- Project_2026_MMP/Controllers/CategoriesController.cs
+++ Project_2026_MMP/Controllers/CategoriesController.cs
@@ -13,10 +13,9 @@ namespace Project_2026_MMP.Controllers
 
         public IActionResult Edit(int? id)
         {
-            var category = new Category { CategoryId = id.HasValue?id.Value : 0  };
+            var category = CategoriesRepository.GetCategoryById(id.HasValue ? id.Value : 0);
 
             return View(category);
-
         }
 
     }
