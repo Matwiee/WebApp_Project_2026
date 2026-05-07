ea5b075b71c9e582e6ed98980ec364a9f6f22540
Matwiee <mateuszwiercioch0@gmail.com>
2026-05-07 15:45:13 +0200

    feat: implement add product feature

--- Project_2026_MMP/Controllers/ProductsController.cs
+++ Project_2026_MMP/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Project_2026_MMP.Models;
+using Project_2026_MMP.ViewModels;
 
 namespace Project_2026_MMP.Controllers
 {
@@ -10,5 +11,27 @@ namespace Project_2026_MMP.Controllers
             var products = ProductsRepository.GetProducts(loadCategory: true);
             return View(products);
         }
-    }
+
+        public IActionResult Add()
+        {
+            var productViewModel = new ProductViewModel
+            {
+                Categories = CategoriesRepository.GetCategories()
+            };
+
+            return View(productViewModel);
+        }
+        [HttpPost]
+        public IActionResult Add(ProductViewModel productViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                ProductsRepository.AddProduct(productViewModel.Product);
+                return RedirectToAction(nameof(Index));
+            }
+
+            productViewModel.Categories = CategoriesRepository.GetCategories();
+            return View(productViewModel);
+        }
+    }   
 }
