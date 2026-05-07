df89448f69e0ec646b3c8787a970057ac033706a
Matwiee <mateuszwiercioch0@gmail.com>
2026-05-07 10:54:07 +0200

    feat: add form validation using Data Annotations

--- Project_2026_MMP/Models/Category.cs
+++ Project_2026_MMP/Models/Category.cs
@@ -1,9 +1,13 @@
-﻿namespace Project_2026_MMP.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Project_2026_MMP.Models
 {
     public class Category
     {
         public int CategoryId { get; set; }
 
+
+        [Required]
         public string Name { get; set; } = string.Empty;
 
         public string Description { get; set; }= string.Empty;
