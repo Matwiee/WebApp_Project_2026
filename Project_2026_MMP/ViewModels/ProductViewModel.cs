a97adc9a7f3b800f0d8d7737df8214a889d6c5ff
Matwiee <mateuszwiercioch0@gmail.com>
2026-05-07 14:43:14 +0200

    feat: Implementing ViewModels file

---
+++ Project_2026_MMP/ViewModels/ProductViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Project_2026_MMP.Models;
+
+namespace Project_2026_MMP.ViewModels
+{
+    public class ProductViewModel
+    {
+        public IEnumerable<Category> Categories { get; set; } = new List<Category>();
+        public Product Product { get; set; } = new Product();
+
+    }
+}
