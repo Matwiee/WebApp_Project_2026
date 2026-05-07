617b0b41477c0be880ac4252f798cdeba1043a59
Marysiaa05 <marysia.egm@gmail.com>
2026-05-07 21:28:07 +0200

    feat: populate categories dropdown and refactor with paritial views

---
+++ Project_2026_MMP/Controllers/SalesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project_2026_MMP.Controllers
+{
+    public class SalesController : Controller
+    {
+        public IActionResult Index()
+        {
+            var salesViewModel = new ViewModels.SalesViewModel
+            {
+                Categories = Models.CategoriesRepository.GetCategories()
+            };
+            return View(salesViewModel);
+        }
+    }
+}
---
+++ Project_2026_MMP/ViewModels/SalesViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Project_2026_MMP.Models;
+
+namespace Project_2026_MMP.ViewModels
+{
+    public class SalesViewModel
+    {
+        public int SelectedCategoryId { get; set; }
+        public IEnumerable<Category> Categories { get; set; } = new List<Category>();
+    }
+}
