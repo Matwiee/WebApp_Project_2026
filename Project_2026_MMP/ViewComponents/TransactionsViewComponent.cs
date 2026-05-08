a83e09a068cc6d66461113a18aa3f60375daca9c
Matwiee <mateuszwiercioch0@gmail.com>
2026-05-08 17:00:52 +0200

    feat: add initial ViewComponent structure

---
+++ Project_2026_MMP/ViewComponents/TransactionsViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project_2026_MMP.ViewComponents
+{
+    [ViewComponent]
+    public class TransactionsViewComponent : ViewComponent
+    {
+        public string Invoke()
+        {
+
+
+            return "List of Transactions";
+        }
+    }
+}
