0eb13bbe676e6182bf9b0d261b727bcbb510a10e
Matwiee <mateuszwiercioch0@gmail.com>
2026-05-08 17:10:54 +0200

    feat: implement Transactions ViewComponent

--- Project_2026_MMP/ViewComponents/TransactionsViewComponent.cs
+++ Project_2026_MMP/ViewComponents/TransactionsViewComponent.cs
@@ -1,15 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Project_2026_MMP.Models;
 
 namespace Project_2026_MMP.ViewComponents
 {
     [ViewComponent]
     public class TransactionsViewComponent : ViewComponent
     {
-        public string Invoke()
+        public IViewComponentResult Invoke(string userName)
         {
+            var transactions = TransactionsRepository.GetByDayAndCashier(userName, DateTime.Now);
 
-
-            return "List of Transactions";
+            return View(transactions);
         }
     }
 }
